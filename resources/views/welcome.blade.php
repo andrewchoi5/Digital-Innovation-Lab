b53c1c2abca4e52939f76b40ade8b70532191e2b
David Park <davidp@ca.ibm.com>
2016-04-15 11:10:23 -0400

    content update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,7 +149,7 @@
                   </a>
                   </td>
                   <td style="width:55%">
-                    <h4 class ="title">Seat Selection in Virtual Reality</h4>
+                    <h4>Seat Selection in Virtual Reality</h4>
                     <h5 class="subtitle">Technologies: Virtual Reality, Unity, Android</h5>
                     <h5 class="subtitle">Industry: Transportation, Entertainment</h5>
                     <br>
@@ -188,7 +188,7 @@
                     <img src="/img/rideportfolio.png" class="img-responsive" alt="ride portfolio">
                   </td>
                   <td style="width:55%">
-              				  <h4 class="title">Ride Your Portfolio VR</h4>
+              				  <h4>Ride Your Portfolio VR</h4>
                         <h5 class="subtitle">Technologies: Virtual Reality, Unity, Android</h5>
                         <h5 class="subtitle">Industry: Banking, Entertainment</h5>
                         <br>
@@ -206,26 +206,40 @@
                           </div>
                         </div>
                   </td>
-                  <td style="width:10%; vertical-align:middle"><span class="btn btn-md btn-warning">Try It</span></td>
+                  <td style="width:10%; vertical-align:middle">
+                    <a href="#">
+                        <span class="btn btn-md btn-warning" style="margin-bottom:15px">Installation Guide</span>
+                    </a>
+                      <span class="btn btn-md btn-success">Feedback</span>
+
+                  </td>
                 </tr>
 
                 <tr>
                   <td style="width:35%">
                     <img src="/img/outofband.png" class="img-responsive" alt="outofband">
                   </td>
-                  <td style="width:55%"><h4>Out of Band Authentication </h4>
+                  <td style="width:55%">
+                    <h4>Out of Band Authentication </h4>
+                    <h5 class="subtitle">Technologies: IOS, Web</h5>
+                    <h5 class="subtitle">Industry: Banking</h5>
+                    <br>
                       <p>Log in to your devices/applications from a single primary device by leveraging Apple's
-                        powerful TouchID biometric authentication system!</p>
-
-                      <p>This application is a POC developed to demonstrate Out-Of-Band-Authentication (OBA)
+                        powerful TouchID biometric authentication system!This application is a POC developed to demonstrate Out-Of-Band-Authentication (OBA)
                         applied to a number of different channels. By using TouchID with the iOS application on a
-                        primary device, you can login to a Web Application, TvOS Application, and iPad application. </p>
-
-                        <p>An additional use case presented in this application is call centre identity verification.
+                        primary device, you can login to a Web Application, TvOS Application, and iPad application.
+                        An additional use case presented in this application is call centre identity verification.
                           Instead of having a call centre agent ask you verbal questions to verify your identity,
                           they can verify your identity with TouchID.</p>
                   </td>
-                  <td style="width:10%; vertical-align:middle"><span class="btn btn-md btn-warning">Try It</span></td>
+                  <td style="width:10%; vertical-align:middle">
+                    <a href="#">
+                        <span class="btn btn-md btn-warning" style="margin-bottom:15px">Installation Guide</span>
+                    </a>
+
+                      <span class="btn btn-md btn-primary" style="margin-bottom:15px">Presentation</span>
+                      <span class="btn btn-md btn-success">Feedback</span>
+                  </td>
                 </tr>
 
                 <tr>
@@ -234,13 +248,17 @@
                     <img src="/img/sensortag_play.png" class="img-responsive" alt="sensortag" data-toggle="modal" data-target="#videoModal1">
                     </a>
                   </td>
-                  <td style="width:55%"><h4>SensorTag Luggage Tracker (IoT)</h4>
+                  <td style="width:55%">
+                    <h4>SensorTag Luggage Tracker</h4>
+                    <h5 class="subtitle">Technologies: IOT, Android</h5>
+                    <h5 class="subtitle">Industry: Transportation</h5>
+                    <br>
                     <p>You've just had a 10-hour, hectic flight from LAX to the Heathrow Airport,
                        and can't wait to take a good couple hours of nap at your hotel.
                        You are so exhausted that you cannot stand in front the carousel,
                         searching for your baggages the old style. Why not just sit down
-                        and let the SensorTag Luggage Tracker show you where they are?</p>
-                      <p>Utilizing Texas Instrument's TI CC2650 SensorTag model, this small,
+                        and let the SensorTag Luggage Tracker show you where they are?
+                        Utilizing Texas Instrument's TI CC2650 SensorTag model, this small,
                         Android prototype of this feature is built for airline customers.
                         For this POC, we've used experimented with many different sensors and algorithms within
                          this powerful chip from Texas Instrument. The concluding product is an ad hoc application
@@ -260,9 +278,12 @@
                           </div>
                   </td>
                   <td style="width:10%; vertical-align:middle">
-                    <a href="/sensortag">
-                        <span class="btn btn-md btn-warning">Try It</span>
+                    <a href="#">
+                        <span class="btn btn-md btn-warning" style="margin-bottom:15px">Installation Guide</span>
                     </a>
+
+                      <span class="btn btn-md btn-primary" style="margin-bottom:15px">Presentation</span>
+                      <span class="btn btn-md btn-success">Feedback</span>
                   </td>
                 </tr>
 
@@ -272,21 +293,27 @@
                     <img src="/img/familybanking_play.png" class="img-responsive" alt="familybanking" data-toggle="modal" data-target="#videoModal2">
                     </a>
                   </td>
-                  <td style="width:55%"><h4>Watson Family Banking</h4>
+                  <td style="width:55%">
+                    <h4>Watson Family Banking</h4>
+                    <h5 class="subtitle">Technologies: IOS, Web, Watson</h5>
+                    <h5 class="subtitle">Industry: Banking</h5>
+                    <br>
                     <p>Regardless of your income, your financial management always has a room to improve.
                        Whether that be reducing the monthly interest payments or searching for a better
                        credit card which better meets your financial requirements, Watson Family Banking
-                       can only serve in a positive way, in an efficient manner.</p>
-                      <p>The Watson Family Banking Application utilizes IBM Watson services and APIs in order
+                       can only serve in a positive way, in an efficient manner.
+                       The Watson Family Banking Application utilizes IBM Watson services and APIs in order
                         to optimize and enhance your family financial management. Using SDK for Node.js as well
                          as Watson's Dialog API, Watson engages in a conversation with you to fulfil your
                          financial management needs. It is built on Bluemix, utilizing the Cloud Foundry,
                           an open source cloud service (PaaS).</p>
                   </td>
                   <td style="width:10%; vertical-align:middle">
-                    <a href="/familybanking">
-                        <span class="btn btn-md btn-warning">Try It</span>
+                    <a href="#">
+                        <span class="btn btn-md btn-warning" style="margin-bottom:15px">Installation Guide</span>
                     </a>
+
+                      <span class="btn btn-md btn-success">Feedback</span>
                   </td>
                 </tr>
               </table>
