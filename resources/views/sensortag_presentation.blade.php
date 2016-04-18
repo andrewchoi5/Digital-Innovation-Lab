13cca6f9d14d9191d07c8dbc47a79fbf3c5d48ab
David Park <davidp@ca.ibm.com>
2016-04-18 13:54:22 -0400

    fix sensortag presentation

--- resources/views/sensortag_presentation.blade.php
+++ resources/views/sensortag_presentation.blade.php
@@ -29,7 +29,7 @@ function detectBrowser()
     var slashIndex = window.location.href.lastIndexOf("/");
     var cropped = window.location.href.slice(0,slashIndex+1);
 
-    window.location.replace("/assets_sensortag/player/KeynoteDHTMLPlayer.html");
+    window.location.replace("/assets_SensorTag/player/KeynoteDHTMLPlayer.html");
 
 }
 
