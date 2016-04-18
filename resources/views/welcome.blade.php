708cf67d1b8fd138a8621ba10ff8a9d99d369e0c
David Park <davidp@ca.ibm.com>
2016-04-18 13:49:04 -0400

    update demo video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
               <div class="modal-body">
                 <div align="center" class="embed-responsive embed-responsive-16by9">
                     <video class="embed-responsive-item" controls>
-                      <source src="/video/seatselection_demo.mov">
+                      <source src="/video/seatselection_demo_low.mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
