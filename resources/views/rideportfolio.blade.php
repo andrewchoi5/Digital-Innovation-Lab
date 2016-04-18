17adcd59082aec211fab5a3aa0f36057f63004b3
David Park <davidp@ca.ibm.com>
2016-04-18 16:13:15 -0400

    style

--- resources/views/rideportfolio.blade.php
+++ resources/views/rideportfolio.blade.php
@@ -12,7 +12,8 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
 <style>
-
+.subtitle {margin-bottom:0; margin-top: 0;}
+.title {margin-bottom:0; }
 body {
 
 
